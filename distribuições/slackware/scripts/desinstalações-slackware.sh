5e12d838e1a22a6d364bcecf32e8145f29ddae62
seilaeu <seilaeu@slackware.pt>
2025-02-05 18:58:58 +0000

    updating file

--- distribuições/slackware/scripts/desinstalações-slackware.sh
+++ distribuições/slackware/scripts/desinstalações-slackware.sh
@@ -1,85 +1,27 @@
 #! /bin/bash
-sudo removepkg /var/log/packages/rust-*
-sudo removepkg /var/log/packages/calligra*
-sudo removepkg /var/lib/pkgtools/packages/wpa_supplicant-*
-sudo removepkg /var/lib/pkgtools/packages/filelight-*
-sudo removepkg /var/lib/pkgtools/packages/glade-*
-sudo removepkg /var/lib/pkgtools/packages/skanlite-*
-sudo removepkg /var/lib/pkgtools/packages/kdenlive-*
-sudo removepkg /var/lib/pkgtools/packages/QtAV-*
-sudo removepkg /var/lib/pkgtools/packages/digikam-*
-sudo removepkg /var/lib/pkgtools/packages/khelpcenter-*
-sudo removepkg /var/lib/pkgtools/packages/krita-*
-sudo removepkg /var/lib/pkgtools/packages/kolourpaint-*
-sudo removepkg /var/lib/pkgtools/packages/kontrast-*
-sudo removepkg /var/lib/pkgtools/packages/kruler-*
-sudo removepkg /var/lib/pkgtools/packages/kopete-*
-sudo removepkg /var/lib/pkgtools/packages/konversation-*
-sudo removepkg /var/lib/pkgtools/packages/plasma-firewall-*
-sudo removepkg /var/lib/pkgtools/packages/dragon-*
-sudo removepkg /var/lib/pkgtools/packages/konqueror-*
-sudo removepkg /var/lib/pkgtools/packages/joe-*
-sudo removepkg /var/lib/pkgtools/packages/lokalize-*
-sudo removepkg /var/log/packages/hplip-*
-sudo removepkg /var/log/packages/falkon-*
-sudo removepkg /var/log/packages/nmap-*
-sudo removepkg /var/log/packages/korganizer-*
-sudo removepkg /var/log/packages/zanshin-*
-sudo removepkg /var/lib/pkgtools/packages/system-config-printer-*
-sudo removepkg /var/lib/pkgtools/packages/cups-*
-sudo removepkg /var/lib/pkgtools/packages/wacomtablet-*
-sudo removepkg /var/lib/pkgtools/packages/fcitx-*
-sudo removepkg /var/lib/pkgtools/packages/anthy-*
-sudo removepkg /var/lib/pkgtools/packages/xf86-input-acecad-*
-sudo removepkg /var/lib/pkgtools/packages/xf86-input-joystick-*
-sudo removepkg /var/lib/pkgtools/packages/kdevelop-*
-sudo removepkg /var/lib/pkgtools/packages/latte-dock-*
-sudo removepkg /var/lib/pkgtools/packages/kmail-*
-sudo removepkg /var/lib/pkgtools/packages/lilo-*
-sudo removepkg /var/lib/pkgtools/packages/elilo-*
-sudo removepkg /var/lib/pkgtools/packages/xf86-input-acecad-*
-sudo removepkg /var/lib/pkgtools/packages/kdevelop-*
-sudo removepkg /var/lib/pkgtools/packages/*gnome*
-sudo removepkg /var/lib/pkgtools/packages/vala-*
-sudo removepkg /var/lib/pkgtools/packages/soma-*
-sudo removepkg /var/lib/pkgtools/packages/seamonkey-*
-sudo removepkg /var/lib/pkgtools/packages/kontactinterface-*
-sudo removepkg /var/lib/pkgtools/packages/kmailtransport-*
-sudo removepkg /var/lib/pkgtools/packages/libksieve-*
-sudo removepkg /var/lib/pkgtools/packages/kdepim-*
-sudo removepkg /var/lib/pkgtools/packages/libkdepim-*
-sudo removepkg /var/lib/pkgtools/packages/pimcommon-*
-sudo removepkg /var/lib/pkgtools/packages/pim-data-exporter-*
-sudo removepkg /var/lib/pkgtools/packages/pim-sieve-editor-*
-sudo removepkg /var/lib/pkgtools/packages/kile-*
-sudo removepkg /var/lib/pkgtools/packages/xterm-*
-sudo removepkg /var/lib/pkgtools/packages/kdf-*
-sudo removepkg /var/lib/pkgtools/packages/ruby-*
-sudo removepkg /var/lib/pkgtools/packages/rubygem-asciidoctor-*
-sudo removepkg /var/lib/pkgtools/packages/kfloppy-*
-sudo removepkg /var/lib/pkgtools/packages/floppy-*
-sudo removepkg /var/lib/pkgtools/packages/plasma-sdk-*
-sudo removepkg /var/lib/pkgtools/packages/print-manager-*
-sudo removepkg /var/lib/pkgtools/packages/gutenprint-*
-sudo removepkg /var/lib/pkgtools/packages/krfb-*
-sudo removepkg /var/lib/pkgtools/packages/krdc-*
-sudo removepkg /var/lib/pkgtools/packages/grantlee-editor-*
-sudo removepkg /var/lib/pkgtools/packages/juk-*
-sudo removepkg /var/lib/pkgtools/packages/umbrello-*
-sudo removepkg /var/lib/pkgtools/packages/kcm-fcitx-*
-sudo removepkg /var/lib/pkgtools/packages/kirigami-*
-sudo removepkg /var/lib/pkgtools/packages/distcc-*
-sudo removepkg /var/lib/pkgtools/packages/cervisia-*
-sudo removepkg /var/lib/pkgtools/packages/akonadiconsole-*
-sudo removepkg /var/lib/pkgtools/packages/kimagemapeditor-*
-sudo removepkg /var/lib/pkgtools/packages/kaddressbook-*
-sudo removepkg /var/lib/pkgtools/packages/liberation-fonts-ttf-*
-sudo removepkg /var/lib/pkgtools/packages/ttf-tlwg-0.7.3-*
-sudo removepkg /var/lib/pkgtools/packages/ttf-indic-fonts-*
-sudo removepkg /var/lib/pkgtools/packages/wqy-zenhei-font-ttf-*
-sudo removepkg /var/lib/pkgtools/packages/php-*
-sudo removepkg /var/lib/pkgtools/packages/imagemagick-*
-sudo removepkg /var/lib/pkgtools/packages/mutt-*
-sudo removepkg /var/lib/pkgtools/packages/mercurial-*
-sudo removepkg /var/lib/pkgtools/packages/merkuro-*
-sudo removepkg /var/lib/pkgtools/packages/plasma-workspace-wallpapers-*
+
+cd /var/lib/pkgtools/packages &&
+sudo removepkg rust-* calligra* wpa_supplicant-* filelight-* glade-* skanlite-* kdenlive-* \
+QtAV-* digikam-* khelpcenter-* krita-* kolourpaint-* kontrast-* kruler-* kopete-* konversation-* \
+plasma-firewall-* dragon-*konqueror-* joe-* lokalize-* hplip-* falkon-* nmap-* korganizer-* \
+zanshin-* system-config-printer-* cups-* wacomtablet-* fcitx5-* anthy-* xf86-input-acecad-* \
+xf86-input-joystick-* kdevelop-* latte-dock-* kmail-* lilo-* elilo-* xf86-input-acecad-* \
+kdevelop-* *gnome* vala-* soma-* seamonkey-* kontactinterface-* kmailtransport-* libksieve-* \
+kdepim-* libkdepim-* pimcommon-* pim-data-exporter-* pim-sieve-editor-* kile-* xterm-* kdf-* \
+ruby-* rubygem-asciidoctor-* kfloppy-* floppy-* plasma-sdk-* print-manager-* gutenprint-* \
+krfb-* krdc-* grantlee-editor-* juk-* umbrello-* kcm-fcitx-* kirigami-* distcc-* cervisia-* \
+akonadiconsole-* kimagemapeditor-* kaddressbook-* liberation-fonts-ttf-* ttf-tlwg-0.7.3-* \
+ttf-indic-fonts-* wqy-zenhei-font-ttf-* php-* imagemagick-* mutt-* mercurial-* merkuro-* \
+plasma-workspace-wallpapers-* MPlayer-* NetworkManager-openvpn-* audacious-* blackbox-* \
+blueman-* ddd-* easytag-* electricsheep-* ffmpegthumbnailer-* fluxbox-* freerdp-* fvwm-* \
+geeqie-* gftp-* gkrellm-* gnuchess-* gnuplot-* gparted-* gucharmap-* gv-* hexchat-* mpv-* \
+mozilla-thunderbird-* pan-* pidgin-* qalculate-* rdesktop-* rxvt-unicode-* seamonkey-* \
+seyon-* ssr-* vim-gvim-* windowmaker-* x11-ssh-askpass-* x3270-* xaos-* xgames-* xine-lib-* \
+xlockmore-* xine-ui-* xmms-* xpaint-* xpdf-* xsane-* xscreensaver-* xsnow-* httpd-* kleopatra-* \
+knotes-* akregator-* mbox-importer-* akonadi-calendar-* libgravatar akonadi-import-wizard-* \
+incidenceeditor-* mailcommon-* messagelib-* kontact-* rust-* kdev-php-* kdev-python-* dvdauthor-* \
+epic5-* step-* dovecot-* subversion-* akonadi-calendar-tools-* kjots-* tokodon-* ibus-anthy-* \
+mailimporter-* cantor-* normalize-* kalarm-* netatalk-* dconf-editor-* labplot-* eventviews-* \
+nut-* calendarsupport-* kross-interpreters-* libhandy
+
+
