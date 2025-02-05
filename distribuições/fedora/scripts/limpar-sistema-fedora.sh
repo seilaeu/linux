71de8346269d560909ca5b9e6b4542702d436321
seilaeu <ribeiro.jose.a.n@gmail.com>
2025-02-05 17:30:50 +0000

    updating file

---
+++ distribuições/fedora/scripts/limpar-sistema-fedora.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+echo "Limpar o fedora"
+
+rm -rf ~/.cache/*
+rm -r ~/.config/chromium/Default/File\ System/*
+rm -r ~/.local/share/akonadi/*
+rm -r ~/.local/share/Trash/*
+sudo rm -r /.Trash*
+sudo rm -rf /usr/tmp/*
+sudo rm -rf /var/cache/*
+sudo rm -rf /var/log/journal/*
+sudo rm -rf /var/tmp/*
+sudo rm -rf /tmp/*
