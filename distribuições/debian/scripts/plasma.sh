573db0265299102b8a12ef7349b1bae8c263aa10
seilaeu <ribeiro.jose.a.n@gmail.com>
2025-02-06 23:27:04 +0000

    updating file

--- distribuições/debian/scripts/plasma.sh
+++ distribuições/debian/scripts/plasma.sh
@@ -1,11 +1,11 @@
 # KDE
-sudo apt install ark elisa discover ktorrent dolphin gwenview \
-kcalc kdeconnect kde-spectacle kfind konsole kmymoney kwrite okular \
-kwin-decoration-oxygen partitionmanager plasma-desktop qapt-deb-installer \
-sddm sddm-theme-breeze sddm-theme-debian-breeze && sudo apt purge \
-amarok gksu kwalletmanager kmail kmousetool kmouth juk kaccessible \
-kmag kaddressbook konqueror korganizer sddm-theme-debian-maui \
-task-portuguese-kde-desktop khelpcenter
+sudo apt install ark elisa ktorrent dolphin gwenview kcalc \
+kdeconnect kde-spectacle kfind konsole kmymoney kwrite okular \
+kwin-decoration-oxygen partitionmanager plasma-desktop sddm \
+sddm-theme-breeze sddm-theme-debian-breeze qapt-deb-installer && \
+sudo apt purge amarok gksu kwalletmanager kmail kmousetool kmouth \
+juk kaccessible kmag kaddressbook konqueror korganizer khelpcenter \
+sddm-theme-debian-maui task-portuguese-kde-desktop
 
 
 
