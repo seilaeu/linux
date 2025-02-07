3ab137dba3ebe17b01da5331a9c57e40756b1fdf
seilaeu <ribeiro.jose.a.n@gmail.com>
2025-02-07 00:20:00 +0000

    updating file

--- distribuições/debian/scripts/pacotes-comuns.sh
+++ distribuições/debian/scripts/pacotes-comuns.sh
@@ -1,6 +1,6 @@
-sudo apt-get install alsa-utils apt-file aptitude bleachbit debian-keyring \
-deborphan dirmngr fakeroot git hunspell-pt-pt hyphen-pt-pt mlocate mythes-pt-pt \
-nano ncdu psmisc pv ttf-bitstream-vera vlc xinit wget youtube-dl firefox-esr-l10n-pt-pt
+sudo apt-get install alsa-utils git hunspell-pt-pt hyphen-pt-pt \
+plocate mythes-pt-pt nano ncdu ttf-bitstream-vera vlc xinit wget \
+yt-dlp firefox-esr-l10n-pt-pt
 
 sudo apt-get purge cups-bsd cups-client fping laptop-detect mutt netselect \
 telnet vim-common vim-tiny plasma-wallpapers-addons wamerican aspell-en \
