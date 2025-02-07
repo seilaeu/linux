5a66bece2a8e530218d3264dfc48a02943829373
seilaeu <ribeiro.jose.a.n@gmail.com>
2025-02-07 17:08:22 +0000

    updating file

--- distribuições/debian/scripts/pacotes-comuns.sh
+++ distribuições/debian/scripts/pacotes-comuns.sh
@@ -1,8 +1,8 @@
-sudo apt-get install alsa-utils git hunspell-pt-pt hyphen-pt-pt \
+sudo apt install aptitude alsa-utils git hunspell-pt-pt hyphen-pt-pt \
 plocate mythes-pt-pt nano ncdu ttf-bitstream-vera vlc xinit wget \
 yt-dlp firefox-esr-l10n-pt-pt
 
-sudo apt-get purge cups-bsd cups-client fping laptop-detect mutt netselect \
+sudo apt purge cups-bsd cups-client fping laptop-detect mutt netselect \
 telnet vim-common vim-tiny plasma-wallpapers-addons wamerican aspell-en \
 avahi-daemon ppp task-portuguese task-portuguese-desktop hunspell-en-us
 
