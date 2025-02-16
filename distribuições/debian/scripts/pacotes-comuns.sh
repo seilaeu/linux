e1dbbfec9188eb01cb2c6185cbff863495931e3f
seilaeu <ribeiro.jose.a.n@gmail.com>
2025-02-16 19:29:02 +0000

    updating file

--- distribuições/debian/scripts/pacotes-comuns.sh
+++ distribuições/debian/scripts/pacotes-comuns.sh
@@ -1,8 +1,11 @@
-sudo apt install aptitude alsa-utils git hunspell-pt-pt hyphen-pt-pt \
-plocate mythes-pt-pt nano ncdu ttf-bitstream-vera vlc xinit wget \
-yt-dlp firefox-esr-l10n-pt-pt
+sudo apt install aptitude apt-file alsa-utils git plocate nano ncdu \
+ttf-bitstream-vera vlc xinit wget yt-dlp firefox-esr-l10n-pt-pt \
+libreoffice-calc libreoffice-writer libreoffice-l10n-pt hunspell-pt-pt \
+hyphen-pt-pt mythes-pt-pt libreoffice-kf6 ttf-mscorefonts-installer kmymoney elisa
 
 sudo apt purge cups-bsd cups-client fping laptop-detect mutt netselect \
 telnet vim-common vim-tiny plasma-wallpapers-addons wamerican aspell-en \
-avahi-daemon ppp task-portuguese task-portuguese-desktop hunspell-en-us
+avahi-daemon ppp task-portuguese task-portuguese-desktop hunspell-en-us \
+libreoffice-java-common libreoffice-math libreoffice-base libreoffice-impress \
+libreoffice-draw libreoffice-help-pt libreoffice-style-tango
 
