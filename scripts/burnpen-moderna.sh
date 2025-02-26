ece83111d826d30309a52c85899d962ff094e838
seilaeu <ribeiro.jose.a.n@gmail.com>
2025-02-26 17:02:45 +0000

    updating file

---
+++ scripts/burnpen-moderna.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash 
+
+if [ $# -ne 2 ]; then
+  echo "Syntax: $0 ficheiro_ISO dispositivo"
+  exit 1
+fi
+
+iso="$1"
+dev="$2"
+
+if [ ! -f "$iso" ]; then
+  echo "Erro: O primeiro argumento deve ser um ficheiro ISO"
+  exit 2
+fi
+
+if [ ! -b "$dev" ]; then
+  echo "Erro: O segundo argumento deve ser um dispositivo"
+  exit 3
+fi
+
+
+sudo dd bs=4M if="$iso" of="$dev" conv=fsync oflag=direct status=progress
+
+echo
+echo "Imagem Gravada no Dispositivo USB"
