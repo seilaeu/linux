6fbd8964bfeaa46921eb54050d0078f975d9db4e
seilaeu <ribeiro.jose.a.n@gmail.com>
2025-03-15 17:37:14 +0000

    updating file

--- distribuições/slackware/scripts/actualizar-slackware.sh
+++ distribuições/slackware/scripts/actualizar-slackware.sh
@@ -1,3 +1,4 @@
 sudo slackpkg update; sudo slackpkg install-new; sudo slackpkg upgrade-all; sudo slackpkg clean-system; sudo sbocheck; sudo sboupgrade --all && echo
+
 sudo find / -not -path '/proc*' -not -path '/run*' -xtype l && echo
 
