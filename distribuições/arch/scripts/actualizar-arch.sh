3e6211438224d6888c8f34ff6763a197ca928851
seilaeu <ribeiro.jose.a.n@gmail.com>
2025-03-18 17:04:54 +0000

    updating file

--- distribuições/arch/scripts/actualizar-arch.sh
+++ distribuições/arch/scripts/actualizar-arch.sh
@@ -1,5 +1,6 @@
-sudo reflector --verbose --country portugal -p https --age 2 --sort rate --save /etc/pacman.d/mirrorlist && echo && sudo pacman -Syu && echo && yay && echo
-sudo pacman -R $(pacman -Qqtd) && echo
-paccache -rk 3 && paccache -ruk 0 && echo
-sudo find / -not -path '/proc*' -not -path '/run*' -xtype l && echo
-systemctl --failed && echo && journalctl -p 3 -b && echo
+sudo reflector --verbose --country portugal -p https --age 2 --sort rate --save /etc/pacman.d/mirrorlist && echo && sudo pacman -Syu && echo && yay && echo 
+sudo pacman -R $(pacman -Qqtd) && echo 
+paccache -rk 3 && paccache -ruk 0 && echo 
+sudo find / -not -path '/proc*' -not -path '/run*' -xtype l && echo 
+systemctl --failed && echo 
+journalctl -p 3 -b && echo
